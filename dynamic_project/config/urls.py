f29c5766c050c2b4c9fc4786a2b9e8da23455ea2
st4lk <myhappydo@gmail.com>
2015-03-11 10:04:07 +0300

    ensure csrf

--- dynamic_project/config/urls.py
+++ dynamic_project/config/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.decorators.csrf import ensure_csrf_cookie
 from apps.auto.views import HomeView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', HomeView.as_view(), name="home"),
+    url(r'^$', ensure_csrf_cookie(HomeView.as_view()), name="home"),
     url(r'^api/', include('apps.auto.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
